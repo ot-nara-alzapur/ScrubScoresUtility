462354c68f4d9c777f6a6a5c8fd373eac927fb89
Nara Alzapur <nalzapur@onetechnologies.net>
2011-06-01 17:08:32 -0500

    utility to scrub scores from log files

---
+++ ScrubScores/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScrubScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Usage();
+                Console.ReadLine();
+            }
+            var success = new Scrubber().ScrubDirectoryPath(args[0]);
+            if (!success) Console.ReadLine();
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("ScrubScores.exe <DirPathToLogFiles>");
+            Console.WriteLine(@"\t example: ScrubScores.exe c:\dir\dir\Logs");
+        }
+    }
+}
---
+++ ScrubScores/Scrubber.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ScrubScores
+{
+    public class Scrubber
+    {
+        private Dictionary<string, string> regExPatternMap;
+
+        public Scrubber()
+        {
+            regExPatternMap = new Dictionary<string, string>
+                                  {
+                                      {"(<[^>]*Score>).*(</[^>]*Score>)", "$1** REMOVED **$2"},
+                                      {"(Score)=\"[^\"]*\"", "$1=\"** REMOVED **\""}
+                                  };
+        }
+
+        public bool ScrubDirectoryPath(string directoryPath)
+        {
+            bool success = true;
+            var files = Directory.GetFiles(directoryPath);
+            foreach (var file in files)
+            {
+                try
+                {
+                    this.ScrubFile(file);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error processing file {0}", file);
+                    Console.WriteLine(ex);
+                    success = false;
+                    continue;
+                }
+            }
+            return success;
+        }
+
+        private void ScrubFile(string filePath)
+        {
+            var fileContents = File.ReadAllText(filePath);
+            regExPatternMap.Keys.ToList()
+                .ForEach(key => fileContents = Regex.Replace(fileContents, key, regExPatternMap[key]));
+            File.WriteAllText(filePath, fileContents);
+        }
+    }
+}
